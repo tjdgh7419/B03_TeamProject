956d9812003161cacc314420b50c29595d7977de
hibernate95 <trytostart95@gmail.com>
2023-08-28 15:06:56 +0900

    인트로 화면
    인트로 화면
    새게임
    로드게임
    종료

---
+++ Dungeon_Battle/Dungeon_Battle/Intro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Battle
+{
+    internal class Intro
+    {
+
+        public void ShowIntro()
+        {
+            Console.Title = "Dungeon_Battle";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("====================================================================================");
+            Console.WriteLine();
+            Console.WriteLine("                            Dungeon_Battle                                          ");
+            Console.WriteLine("DOWA team");
+            Console.WriteLine("====================================================================================");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                               1. New Game");
+            Console.WriteLine();
+            Console.WriteLine("                               2. Load Game");
+            Console.WriteLine();
+            Console.WriteLine("                               3. Quit");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("====================================================================================");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    NewGame();
+                    break;
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    LoadGame();
+                    break;
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    QuitGame();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("잘못된 입력입니다.");
+                    ShowIntro();
+                    break;
+            }
+        }
+
+        private static void NewGame()
+        {
+            Console.WriteLine("새로운 게임 시작하는 중...");
+
+        }
+
+        private static void LoadGame()
+        {
+            Console.WriteLine("세이브 된 파일 불러오는중...");
+        }
+
+        private static void QuitGame()
+        {
+            Console.WriteLine("게임 나가는중...");
+            Environment.Exit(0);
+        }
+    }
+
+}
--- Dungeon_Battle/Dungeon_Battle/Program.cs
+++ Dungeon_Battle/Dungeon_Battle/Program.cs
@@ -4,7 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");			
-		}
+			Intro intro = new Intro();
+			intro.ShowIntro();
+        }
 	}
 }
