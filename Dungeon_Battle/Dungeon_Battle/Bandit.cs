d6deb71955738a85b7a2947aa4e3b96f9c10b4a2
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-29 13:56:29 +0900

    [ADD]도적 클래스 스킬1,2 구현

--- Dungeon_Battle/Dungeon_Battle/Bandit.cs
+++ Dungeon_Battle/Dungeon_Battle/Bandit.cs
@@ -12,18 +12,29 @@ namespace Dungeon_Battle
 		public string skill2 { get; set; }
 		public string skill1Info { get; set; }
 		public string skill2Info { get; set; }
+		public int skill1Cost { get; set; }
+		public int skill2Cost { get; set; }
 		public Bandit()
 		{
 			skill1 = "찌르기";
 			skill2 = "숨기";
 			skill1Info = "공격력 * 1.3으로 하나의 적을 공격합니다.";
 			skill2Info = "회피율이 20% 증가합니다.";
+			skill1Cost = 10;
+			skill2Cost = 20;
 		}
 
 		DisplayGameIntro dp = DisplayGameIntro.Instance();
 		public int skill_1()
 		{
+			dp.player.Mp -= skill1Cost;
 			return (int)(dp.player.Atk * 1.3);
+		}
+
+		public void skill_2()
+		{
+			dp.player.Mp -= skill2Cost;
+			dp.player.Avoidance += 2;
 		}
 	}
 }
