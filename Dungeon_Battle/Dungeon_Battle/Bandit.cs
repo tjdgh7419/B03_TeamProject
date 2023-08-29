ad5e712221615e17f118619f37fa4230523584ca
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-29 13:44:33 +0900

    [ADD] 도적 스킬, 정보 프로퍼티 추가

--- Dungeon_Battle/Dungeon_Battle/Bandit.cs
+++ Dungeon_Battle/Dungeon_Battle/Bandit.cs
@@ -8,5 +8,21 @@ namespace Dungeon_Battle
 {
 	internal class Bandit
 	{
+		public string skill1 { get; set; }
+		public string skill2 { get; set; }
+		public string skill1Info { get; set; }
+		public string skill2Info { get; set; }
+		public Bandit()
+		{
+			skill1 = "찌르기";
+			skill2 = "숨기";
+			skill1Info = "공격력 * 1.3으로 하나의 적을 공격합니다.";
+			skill2Info = "회피율이 20% 증가합니다.";
+		}
+
+		public void skill_1()
+		{
+
+		}
 	}
 }
