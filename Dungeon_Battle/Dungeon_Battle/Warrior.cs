f5db27557e4b6ddf7bba18dfa3a8b06c25c2f796
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-29 14:58:14 +0900

    [ADD] 전사 스킬2 기능 구현

--- Dungeon_Battle/Dungeon_Battle/Warrior.cs
+++ Dungeon_Battle/Dungeon_Battle/Warrior.cs
@@ -2,11 +2,135 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Dungeon_Battle
 {
 	internal class Warrior
 	{
+		public string skill1 { get; set; }
+		public string skill2 { get; set; }
+		public string skill1Info { get; set; }
+		public string skill2Info { get; set; }
+		public int skill1Cost { get; set; }
+		public int skill2Cost { get; set; }
+		public Warrior()
+		{
+			skill1 = "알파 스트라이크";
+			skill2 = "더블 스트라이크";
+			skill1Info = "공격력 * 2 로 하나의 적을 공격합니다.";
+			skill2Info = "공격력 * 1.5 로 2명의 적을 랜덤으로 공격합니다.";
+			skill1Cost = 10;
+			skill2Cost = 15;
+		}
+
+		DisplayGameIntro dp = DisplayGameIntro.Instance();
+		public int skill_1()
+		{
+			dp.player.Mp -= skill1Cost;
+			return (int)(dp.player.Atk * 2);
+		}
+
+		public void skill_2()
+		{
+			Dungeon dg = new Dungeon(dp.player.Hp);
+			dp.player.Mp -= skill2Cost;
+			int monsterHp = 0;
+			double error = 0;
+			int Error = 0;
+			int player_Atk = 0;
+			int monsterCnt = 0;
+			var deadExclude = new HashSet<int>();
+			List<int> liveMonster = new List<int>();
+			Random playerAtk = new Random();
+			for (int i = 0; i < dp.monsterlist.Count; i++)
+			{
+				if (dp.monsterlist[i].Hp < 0)
+				{
+					deadExclude.Add(i);
+				}
+				else
+				{
+					monsterCnt++;
+					liveMonster.Add(i);
+				}
+			}
+
+			if (monsterCnt <= 2)
+			{
+				foreach (int num in liveMonster)
+				{
+					monsterHp = dp.monsterlist[num].Hp;
+					error = (dp.player.Atk / (double)100) * 10;
+					Error = (int)Math.Ceiling(error);
+					player_Atk = playerAtk.Next(dp.player.Atk - Error, dp.player.Atk + Error + 1);
+					dp.monsterlist[num].Hp -= player_Atk;
+					Console.Clear();
+					Console.WriteLine("Battle!!");
+					Console.WriteLine();
+					Console.WriteLine($"{dp.player.Name} 의 공격!");
+					Console.WriteLine($"Lv.{dp.monsterlist[num].Level} {dp.monsterlist[num].Name} 을(를) 맞췄습니다. [데미지 : {player_Atk}]");
+					Console.WriteLine();
+					Console.WriteLine($"Lv.{dp.monsterlist[num].Level} {dp.monsterlist[num].Name}");
+					if (dp.monsterlist[num].Hp > 0) Console.WriteLine($"HP {monsterHp} -> {dp.monsterlist[num].Hp}");
+					else Console.WriteLine($"HP {monsterHp} -> Dead");
+					Console.WriteLine();
+					Console.WriteLine("0. 다음");
+					Console.WriteLine();
+					Console.Write(">>");
+
+					int input1 = dp.CheckValidInput(0, 0);
+
+					switch (input1)
+					{
+						case 0: break;
+					}
+				}
+			}
+			else
+			{
+				for (int i = 0; i < 2; i++)
+				{
+					var range = Enumerable.Range(0, dp.monsterlist.Count + 1).Where(i => !deadExclude.Contains(i));
+					var monRand = new System.Random();
+					int index = monRand.Next(0, (dp.monsterlist.Count + 1) - deadExclude.Count);
+					int monVal = range.ElementAt(index);
+
+					monsterHp = dp.monsterlist[monVal].Hp;
+					error = (dp.player.Atk / (double)100) * 10;
+					Error = (int)Math.Ceiling(error);
+					player_Atk = playerAtk.Next(dp.player.Atk - Error, dp.player.Atk + Error + 1);
+					dp.monsterlist[monVal].Hp -= player_Atk;
+					Console.Clear();
+					Console.WriteLine("Battle!!");
+					Console.WriteLine();
+					Console.WriteLine($"{dp.player.Name} 의 공격!");
+					Console.WriteLine($"Lv.{dp.monsterlist[monVal].Level} {dp.monsterlist[monVal].Name} 을(를) 맞췄습니다. [데미지 : {player_Atk}]");
+					Console.WriteLine();
+					Console.WriteLine($"Lv.{dp.monsterlist[monVal].Level} {dp.monsterlist[monVal].Name}");
+					if (dp.monsterlist[monVal].Hp > 0) Console.WriteLine($"HP {monsterHp} -> {dp.monsterlist[monVal].Hp}");
+					else Console.WriteLine($"HP {monsterHp} -> Dead");
+					Console.WriteLine();
+					Console.WriteLine("0. 다음");
+					Console.WriteLine();
+					Console.Write(">>");
+
+					int input2 = dp.CheckValidInput(0, 0);
+
+					switch (input2)
+					{
+						case 0: break;
+					}
+				}
+			}
+
+			int input = dp.CheckValidInput(0, 0);
+
+			switch (input)
+			{
+				case 0: dg.MonsterAtkStage(); break;
+			}
+		}
 	}
 }
