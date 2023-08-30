5d3175f726f91c2536169c2315fdb16af9704f94
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-30 12:41:32 +0900

    [ADD] stage2 구현

--- Dungeon_Battle/Dungeon_Battle/Stage.cs
+++ Dungeon_Battle/Dungeon_Battle/Stage.cs
@@ -63,21 +63,45 @@ namespace Dungeon_Battle
 			Console.WriteLine("Battle!!");
 			Console.WriteLine();
 			Random monRan = new Random();
-			List<int> monList = new List<int>();
+			List<Monster> monList = new List<Monster>();
+			var normalSet = new HashSet<int>();
+			var hardSet = new HashSet<int>();
+			int monLvlChk = monRan.Next(0, 2);
 
-			for (int i = 0; i < 3; i++)
+			if(monLvlChk == 0)
 			{
-				int ran = monRan.Next(dp.allMonsterlist.Count);
-				monList.Add(ran);
+				for (int i = 0; i < 2; i++)
+				{
+					int a = monRan.Next(0, dp.monsterlist.Count - normalSet.Count);
+					var nRange = Enumerable.Range(0, dp.monsterlist.Count).Where(i => !normalSet.Contains(i));
+					int nVal = nRange.ElementAt(a);
+					monList.Add(dp.monsterlist[nVal]);
+					normalSet.Add(nVal);
+				}
+				int b = monRan.Next(0, dp.hardMonsterlist.Count);
+				monList.Add(dp.hardMonsterlist[b]);
+			}
+			else
+			{
+				for (int i = 0; i < 2; i++)
+				{
+					int c = monRan.Next(0, dp.hardMonsterlist.Count - hardSet.Count);
+					var hRange = Enumerable.Range(0, dp.hardMonsterlist.Count).Where(i => !hardSet.Contains(i));
+					int hVal = hRange.ElementAt(c);
+					monList.Add(dp.hardMonsterlist[hVal]);
+					hardSet.Add(hVal);
+				}
+				int d = monRan.Next(0, dp.monsterlist.Count);
+				monList.Add(dp.monsterlist[d]);
 			}
 			
-			foreach (int val in monList) 
+			foreach (Monster val in monList) 
 			{
-				if (dp.allMonsterlist[val].Hp > 0)
-					Console.WriteLine($"Lv.{dp.allMonsterlist[val].Level} {dp.allMonsterlist[val].Name} HP {dp.allMonsterlist[val].Hp}");
+				if (val.Hp > 0)
+					Console.WriteLine($"Lv.{val.Level} {val.Name} HP {val.Hp}");
 				else
 				{
-					Console.WriteLine($"Lv.{dp.allMonsterlist[val].Level} {dp.allMonsterlist[val].Name} Dead");
+					Console.WriteLine($"Lv.{val.Level} {val.Name} Dead");
 				}
 			}
 			Console.WriteLine();
